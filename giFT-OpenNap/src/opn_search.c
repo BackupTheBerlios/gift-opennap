be9c270cc79c51e83d0b3b611896d7c77df8286b
tsauerbeck <tsauerbeck>
2003-07-06 16:42:22 +0000

    Fixed indentation

--- giFT-OpenNap/src/opn_search.c
+++ giFT-OpenNap/src/opn_search.c
@@ -129,8 +129,8 @@ BOOL opn_search_reply_add(char *file, OpnUrl *url, Share *share)
 	return added;
 }
 
-BOOL opennap_search(Protocol *p, IFEvent *event, char *query, char *exclude,
-                    char *realm, Dataset *meta)
+BOOL opennap_search(Protocol *p, IFEvent *event, char *query,
+                    char *exclude, char *realm, Dataset *meta)
 {
 	OpnSearch *search;
 	OpnPacket *packet;
